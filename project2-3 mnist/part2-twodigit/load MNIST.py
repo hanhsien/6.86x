c2d7c435ed7e15615e9ac62e50618fab67fef72e
Han Hsien <hh.seah@gmail.com>
2019-07-26 17:41:40 -0500

    Update load MNIST.py

--- project2-3 mnist/part2-twodigit/load MNIST.py
+++ project2-3 mnist/part2-twodigit/load MNIST.py
@@ -11,8 +11,8 @@ import numpy
 
 with gzip.open('../Datasets/mnist.pkl.gz', 'rb') as f:
     u = pickle.load(f, encoding = 'latin1')
-    print(p)
-    train_set, valid_set, test_set = p
+    print(u)
+    train_set, valid_set, test_set = u
     
 train_x, train_y = train_set
 
@@ -23,6 +23,8 @@ import matplotlib.pyplot as plt
 
 plt.imshow(train_x[0].reshape((28, 28)), cmap=cm.Greys_r)
 plt.show()
+
+
 
 
 f = gzip.open(path_to_data_dir + 'train_multi_digit' + '_mini' + '.pkl.gz', 'rb')
